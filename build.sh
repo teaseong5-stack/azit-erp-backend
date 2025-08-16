09d4f17d23079bcee34bf7bc4cc3a611bc381680
AZIT-TRAVEL <teaseong5@gmail.com>
2025-08-16 09:33:40 +0700

    Fix gunicorn path in build script

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# 3. 서버 시작 (이 줄이 추가되었습니다)
-gunicorn azit_erp_backend.wsgi:application
+# 3. 서버 시작 (python -m 을 추가하여 수정)
+python -m gunicorn azit_erp_backend.wsgi:application
