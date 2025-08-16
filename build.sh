f20956a2041df245609face8c38b79216cc6e2ac
AZIT-TRAVEL <teaseong5@gmail.com>
2025-08-16 09:22:41 +0700

    Update build script to start server

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# 1. 의존성 패키지 설치
 pip install -r requirements.txt
+
+# 2. Django 관리 명령어 실행
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# 3. 서버 시작 (이 줄이 추가되었습니다)
+gunicorn azit_erp_backend.wsgi:application
