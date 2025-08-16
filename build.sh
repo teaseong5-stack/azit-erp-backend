b91518b5d3af40e87c8db36d55e43deb7c88a419
AZIT-TRAVEL <teaseong5@gmail.com>
2025-08-16 13:17:50 +0700

    Separate build and start commands

--- build.sh
+++ build.sh
@@ -7,6 +7,3 @@ pip install -r requirements.txt
 # 2. Django 관리 명령어 실행
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# 3. 서버 시작 (python -m 을 추가하여 수정)
-python -m gunicorn azit_erp_backend.wsgi:application
