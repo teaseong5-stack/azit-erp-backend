b87e05ad1447f1be8d7e7f304601c58bfedce7f7
AZIT-TRAVEL <teaseong5@gmail.com>
2025-08-18 17:16:43 +0700

    버그 수정: 로그인 API 경로 및 관리자 페이지 모델 등록

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Customer, Reservation # models.py에서 Customer와 Reservation을 가져옴
+from .models import Customer, Reservation, Partner, Transaction
 
-# 관리자 페이지에 Customer와 Reservation 모델을 등록
+# 관리자 페이지에 모델들을 등록합니다.
 admin.site.register(Customer)
 admin.site.register(Reservation)
+admin.site.register(Partner)
+# 누락되었던 Transaction 모델을 추가로 등록합니다.
+admin.site.register(Transaction)
